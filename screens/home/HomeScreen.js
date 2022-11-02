6c7a2a3e36aa2b34b6dd67095808e992aff355c6
elviswlima <elvis.wlima@hotmail.com>
2022-11-02 02:15:41 -0300

    test

--- screens/home/HomeScreen.js
+++ screens/home/HomeScreen.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-import { Container, Title } from './home-style';
+import { Container } from './stylehome';
 import BtnConfirm from '../../components/btnConfirm/BtnConfirmComponent';
-import { Text } from 'react-native';
+import { Text } from 'react-native'
 
 function HomeScreen() {
     return(
         <Container>
-            <Title>Rosa BarberShop</Title>
-            <BtnConfirm>Agendar Serviço</BtnConfirm>
-            <BtnConfirm>Agendamentos</BtnConfirm>
-            <BtnConfirm>Configurações</BtnConfirm>
-            <BtnConfirm>Infos</BtnConfirm>
-        </Container>
+            <Text>Rosa BarberShop</Text>
+            <BtnConfirm title="Agendar Serviço"/>
+            <BtnConfirm title="Agendamentos"/>
+            <BtnConfirm title="Configurações"/>
+            <BtnConfirm title="Infos"/>
+        </ Container>
     )
 }
 
--- screens/home/stylehome.js
+++ screens/home/stylehome.js
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import styled from 'styled-components/native';
+import Colors from '../../constants/Theme';
 
 export const Container = styled.View`
     flex: 1;
     background-color: ${Colors.Light};
-`;
-
-export const Title = styled.View`
-    color: #000;
 `;
