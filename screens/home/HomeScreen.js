8080e51521256e866e3adf3a4c842941ae97eef9
elviswlima <elvis.wlima@hotmail.com>
2022-11-02 16:57:00 -0300

    review code

--- screens/home/HomeScreen.js
+++ screens/home/HomeScreen.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
-import { Container } from './stylehome';
+import { Container, Head } from './home-style';
 import BtnConfirm from '../../components/btnConfirm/BtnConfirmComponent';
 import { Text } from 'react-native'
 
 function HomeScreen() {
     return(
         <Container>
+          <Head>
             <Text>Rosa BarberShop</Text>
-            <BtnConfirm title="Agendar Serviço"/>
-            <BtnConfirm title="Agendamentos"/>
-            <BtnConfirm title="Configurações"/>
-            <BtnConfirm title="Infos"/>
+          </ Head>
+          <BtnConfirm title="Agendar Serviço"/>
+          <BtnConfirm title="Agendamentos"/>
+          <BtnConfirm title="Configurações"/>
+          <BtnConfirm title="Infos"/>
         </ Container>
     )
 }
--- screens/home/stylehome.js
+++ screens/home/stylehome.js
@@ -1,8 +1,20 @@
 import * as React from 'react';
 import styled from 'styled-components/native';
-import Colors from '../../constants/Theme';
 
 export const Container = styled.View`
     flex: 1;
-    background-color: ${Colors.Light};
+    background-color: #fff;
+`;
+
+export const Head = styled.View`
+    flex-direction: row;
+    align-self: center;
+    align-items: center;
+    justify-content: center;
+    font-size: 50px;
+
+    width: 100%;
+    height: 200px;
+
+    margin-top: 20px;
 `;
